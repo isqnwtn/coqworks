e957e80a931854d21459b9e61b58908ab1d303b5
Vismay Raj <vismayraj1@gmail.com>
2022-02-07 19:16:54 +0530

    added nand and proofs

--- basic/first.v
+++ basic/first.v
@@ -8,6 +8,19 @@ Definition negb (b : boolean) : boolean :=
   | false => true
   end.
 
+Definition andb (a b:boolean) : boolean :=
+  match a with
+  | true => b
+  | false => false
+  end.
+
+Definition nandb (a b:boolean): boolean :=
+  match a with
+  | true => negb(b)
+  | false => true
+  end.
+
+
 Theorem negb_negb : forall (b: boolean), negb(negb b) = b.
 Proof.
 intros.
@@ -16,4 +29,29 @@ simpl.
 reflexivity.
 simpl.
 reflexivity.
+Qed.
+
+Theorem false_nand: forall a b: boolean, nandb a b = false -> a=true /\ b=true.
+Proof.
+intros.
+destruct a.
+split.
+reflexivity.
+destruct b.
+reflexivity.
+simpl in H.
+symmetry.
+apply H.
+destruct b.
+simpl in H.
+split.
+symmetry.
+apply H.
+reflexivity.
+simpl in H.
+split.
+symmetry.
+apply H.
+symmetry.
+apply H.
 Qed.
